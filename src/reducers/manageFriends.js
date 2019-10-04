ea02e68207c23336ad160ac96d6f0ace0a8f6e66
ayerest <irisiseve@gmail.com>
2019-10-04 10:16:35 -0700

    finished redux reducer lab

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
+    switch(action.type) {
+        case "ADD_FRIEND":
+            // let newFriend = {name: action.friend.name, hometown: action.friend.hometown, id: action.friend.id}
+            // console.log(newFriend)
+            return {...state, friends: [...state.friends, action.friend]}
+        case "REMOVE_FRIEND": 
+            let myFriends = [...state.friends]
+            // console.log(myFriends)
+            // let filtered = myFriends.filter(friend => {
+
+            // })
+            return {...state, friends: myFriends.filter(friend => {
+                return friend.id !== action.id
+            })}
+        default: 
+            return state
+    }
 }
